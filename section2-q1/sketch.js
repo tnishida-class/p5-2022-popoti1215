22193d9e63f7732c41037674053cd86a0db6965d
popoti1215 <115407896+popoti1215@users.noreply.github.com>
2022-10-18 02:02:44 +0900

    Change section2-1,2-2

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,13 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
+  createCanvas(100,100);
+  noFill();
+  for(let x = 50; x >=30; x=x-5){
+    stroke(255, 0, 0);
+    ellipse(50, 50, x);
+  }
+  for(let y = 25; y >=5; y=y-5){
+    stroke(0, 0, 255);
+    ellipse(50, 50, y);
   }
 }
