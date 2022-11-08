1e93b506b6bd5478db77a58a895ff04c5f8b2b31
popoti1215 <115407896+popoti1215@users.noreply.github.com>
2022-11-08 12:22:32 +0900

    section4-1

--- section4-1/sketch.js
+++ section4-1/sketch.js
@@ -6,42 +6,60 @@ function setup(){
   // 配列をランダムに初期化する
   let scores = [];
   for(let i = 0; i < 10; i++){
-    scores[i] = random(20, 100); // 60以上100未満のランダムな数を代入
-  }
+    scores[i] = random(60, 100); // 60以上100未満のランダムな数を代入
+  }//スコアを10個、20~100までの中でランダムに選ぶ
   console.log(scores);
 
   // 合計を計算する
   let sum = 0;
   for(let i = 0; i < scores.length; i++){
-    sum += scores[i];
+    sum += scores[i];　//sum = sum + scores[i]
   }
   console.log(sum);
 
   // ここから平均・最大・最小を求めます
   let average, largest, smallest;
   // BLANK[1]　平均値（ヒント average = 合計 / 配列の長さ）
+  average = sum / scores.length
+  console.log(average);
 
   largest = 0;
   for(let i = 0; i < scores.length; i++){
-    // BLANK[2]　ヒント：今までの最大値 largest と scores[i] を比較する
+    // BLANK[2]　ヒント：今までの最大値 largest と scores[i] を比較する 
+    if(scores[i] > largest) {largest = scores[i];}
+    else{largest = largest;}
   }
+  console.log(largest);
 
   smallest = 100;
   for(let i = 0; i < scores.length; i++){
     // BLANK[3]　ヒント：最小値とだいたい同じ
+    if(scores[i] < smallest) {smallest = scores[i];}
+    else{smallest = smallest;}
   }
+  console.log(smallest);
 
   // ここから棒グラフを描いていきます。まずは背景に横線をn本引く
   const n = 10;
   for(let i = 0; i < n; i++){ line(0, height * i / n, width, height * i / n); }
-  
+  //line(始点のx座標, 始点のy座標, 終点のx座標, 終点のy座標)
   noStroke();
 
   for(let i = 0; i < scores.length; i++){
     const dx = width / scores.length;
     const h = height * scores[i] / 100;
     // BLANK[4] ヒント: 条件分岐を使って色を変更します
+    if(scores[i]==largest){
+      fill(252,19,3)
+    }
+    else if(scores[i]==smallest){
+      fill(15, 3, 252)
+    }
+    else {
+      fill(114, 114, 117)
+    }
     rect(i * dx + 2, height - h, dx - 4, h);
+    //rect(始点のx座標, 始点のy座標, 横幅, 縦幅)
     fill(0);
     text(scores[i].toPrecision(3), i * dx, height - h);
   }
