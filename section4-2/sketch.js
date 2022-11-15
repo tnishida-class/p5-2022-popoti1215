6f8e9698d9b73f2755001b481098d339ed1e9b8e
popoti1215 <115407896+popoti1215@users.noreply.github.com>
2022-11-15 17:44:42 +0900

    4-2

--- section4-2/sketch.js
+++ section4-2/sketch.js
@@ -3,16 +3,22 @@
 // 練習問題：何も操作しなくてもボールが湧いてくる機能を追加しよう
 
 let balls;
+let size;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
   balls = [];
+  size = [];
+  for(let j = 0; j < size.length; j++){
+    size.push(random(20, 100));
+  }
 }
 
 function draw(){
   background(160, 192, 255);
   for(let i = 0; i < balls.length; i++){
-    let b = balls[i];
+    let b = balls[i]; 
+    let size = size[j];
     ellipse(b.x, b.y, b.size);
     b.x += b.vx;
     b.y += b.vy;
@@ -23,7 +29,7 @@ function mouseDragged(){
   const dx = mouseX - pmouseX;
   const dy = mouseY - pmouseY;
   if(mag(dx, dy) > 5){
-    const b = { x: mouseX, y: mouseY, size: 20, vx: dx, vy: dy };
+    const b = { x: mouseX, y: mouseY, size:20 , vx: dx, vy: dy };
     balls.push(b);
   }
 }
