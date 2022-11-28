178a881466ec35d0eb046d1bda25b48cd07ed517
popoti1215 <115407896+popoti1215@users.noreply.github.com>
2022-11-28 13:29:40 +0900

    5-2 ①

--- section5-2/sketch.js
+++ section5-2/sketch.js
@@ -7,6 +7,7 @@ function setup(){
   crossmark(10, 10, 90, 90);
   ngmark(150, 50, 80);
   star(250, 50, 40);
+  regularPolygon(10, 250, 50, 40);
 }
 
 function crossmark(x1, y1, x2, y2){
@@ -33,4 +34,16 @@ function star(cx, cy, r){
     vertex(x,y);
   }
   endShape(CLOSE);
-}
+} 
+
+function regularPolygon(n, cx, cy, r);{
+  beginShape();
+  for(var i = 0; i < 15; i++){
+    let theta = TWO_PI * i * 2 / 5 - HALF_PI;
+    let x = cx + cos(theta) * r;
+    let y = cy + sin(theta) * r;
+    vertex(x,y);
+  }
+  endShape(CLOSE);
+} 
+
