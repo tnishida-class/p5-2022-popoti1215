1266bc05a346fe172143d82703aaf958f50bfd31
popoti1215 <115407896+popoti1215@users.noreply.github.com>
2022-12-21 15:15:09 +0900

    5-1 EU

--- section5-1/sketch.js
+++ section5-1/sketch.js
@@ -1,11 +1,11 @@
 // テキスト「関数を使う」
 // 練習問題：このプログラムを改造してEUの旗を描いてみよう
 function setup(){
-  createCanvas(200, 200);
+  createCanvas(300, 200);
   background(36, 56, 166);
   for(let i = 0; i < 12; i++){
     let theta = TWO_PI * i / 12;
-    let x = 100 + cos(theta) * 50;
+    let x = 150 + cos(theta) * 50;
     let y = 100 + sin(theta) * 50;
     fill(252, 236, 56);
     star(x, y, 10);
